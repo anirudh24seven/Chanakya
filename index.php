297fd5cb394fbfb21627721bb634420ede4337bc
Anirudh S <anirudh24seven@gmail.com>
2011-04-14 08:34:02 +0530

    Added a basic login form, with simple css to go along with it

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
 <h1>Chanakya</h1>
 <p id="tagline">Employee Performance Management System</p>
 <br />
+<table id="logintable">
+<tr><td>
 <form action="" method="post">
+<br />
 username: <input type="text" /><br />
-password: <input type="password" /><br />
-<input type="submit" value="Login" />
+password: <input type="password" /><br /><br />
+<input type="submit" value="Login" class="button"/>
+<br />
 </form>
+</td></tr>
+</table>
 </body>
 </html>
