c08002d886c71fa5702a727254ecf77b8e6ae03c
Anirudh S <anirudh24seven@gmail.com>
2011-04-14 08:04:31 +0530

    connected index.php with main.css and also started work on index.php

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <title>Chanakya</title>
 <head></head>
 <body>
+<link rel="stylesheet" type="text/css" href="main.css" />
 <h1>Chanakya</h1>
+<p id="tagline">Employee Performance Management System</p>
+<br />
+<form action="" method="post">
+username: <input type="text" /><br />
+password: <input type="password" /><br />
+<input type="submit" value="Login" />
+</form>
 </body>
 </html>
